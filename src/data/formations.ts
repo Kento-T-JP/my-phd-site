20ac08a68684cdcde092cd6b5e8b3d5f2a4b236f
Kento-T-JP <kento0901910@gmail.com>
2025-07-14 19:21:17 +0900

    adjust the player width

--- src/data/formations.ts
+++ src/data/formations.ts
@@ -3,28 +3,28 @@ import type { Formation } from "@/types/formation";
 export const formation433 = {
   name: "4-3-3",
   positions: {
-    GK: { top: 90, left: 50, max: 1 },
-    LB: { top: 70, left: 20, max: 1 },
-    CB: { top: 70, left: 50, max: 2 },
-    RB: { top: 70, left: 80, max: 1 },
-    CMF: { top: 50, left: 50, max: 3 },
-    LW: { top: 30, left: 20, max: 1 },
-    CF: { top: 20, left: 50, max: 1 },
-    RW: { top: 30, left: 80, max: 1 },
+    GK: { top: 85, left: 50, max: 1 },
+    LB: { top: 65, left: 20, max: 1 },
+    CB: { top: 65, left: 50, max: 2 },
+    RB: { top: 65, left: 80, max: 1 },
+    CMF: { top: 45, left: 50, max: 3 },
+    LW: { top: 25, left: 20, max: 1 },
+    CF: { top: 15, left: 50, max: 1 },
+    RW: { top: 25, left: 80, max: 1 },
   },
 } as const satisfies Formation;
 
 export const formation442 = {
   name: "4-4-2",
   positions: {
-    GK: { top: 90, left: 50, max: 1 },
-    LB: { top: 70, left: 20, max: 1 },
-    CB: { top: 70, left: 50, max: 2 },
-    RB: { top: 70, left: 80, max: 1 },
-    LM: { top: 40, left: 20, max: 1 },
-    CMF: { top: 45, left: 50, max: 2 },
-    RM: { top: 40, left: 80, max: 1 },
-    CF: { top: 20, left: 50, max: 2 },
+    GK: { top: 85, left: 50, max: 1 },
+    LB: { top: 65, left: 20, max: 1 },
+    CB: { top: 65, left: 50, max: 2 },
+    RB: { top: 65, left: 80, max: 1 },
+    LM: { top: 35, left: 20, max: 1 },
+    CMF: { top: 40, left: 50, max: 2 },
+    RM: { top: 35, left: 80, max: 1 },
+    CF: { top: 15, left: 50, max: 2 },
   },
 } as const satisfies Formation;
 
@@ -32,18 +32,18 @@ export const formation4231 = {
   name: "4-2-3-1",
   positions: {
     // Back line
-    GK: { top: 90, left: 50, max: 1 },
-    LB: { top: 70, left: 20, max: 1 },
-    CB: { top: 70, left: 50, max: 2 },
-    RB: { top: 70, left: 80, max: 1 },
+    GK: { top: 85, left: 50, max: 1 },
+    LB: { top: 65, left: 20, max: 1 },
+    CB: { top: 65, left: 50, max: 2 },
+    RB: { top: 65, left: 80, max: 1 },
     // Double pivot
-    DMF: { top: 52, left: 50, max: 2 },
+    DMF: { top: 47, left: 50, max: 2 },
     // Attacking midfield three
-    LW: { top: 35, left: 25, max: 1 },
-    CMF: { top: 35, left: 50, max: 1 },
-    RW: { top: 35, left: 75, max: 1 },
+    LW: { top: 30, left: 25, max: 1 },
+    CMF: { top: 30, left: 50, max: 1 },
+    RW: { top: 30, left: 75, max: 1 },
     // Lone striker
-    CF: { top: 15, left: 50, max: 1 },
+    CF: { top: 10, left: 50, max: 1 },
   },
 } as const satisfies Formation;
 
